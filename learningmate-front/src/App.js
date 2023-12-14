0350d4ca563a45541552924fd2a06490f4223f9a
mkhajiit <mkhajiit15@gmail.com>
2023-12-14 12:47:14 +0900

    test commit

---
+++ learningmate-front/src/App.js
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+function App() {
+    const [data, setData] = useState({});
+    const [isloading, setLoading] = useState(true);
+    const getData = useCallback(async () => {
+        try {
+            const resp = await axios.get('http://localhost:8000/test/data');
+            console.log(resp.data);
+            if (resp.data.resdata === false) window.alert('불러오기 실패');
+            else setData((data) => ({ ...data, ...resp.data.resdata }));
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+    useEffect(() => {
+        getData();
+    }, [getData]);
+    if (isloading) {
+        return <div>Loading....</div>;
+    } else {
+        return (
+            <>
+                <h1>test</h1>
+                {/* John Doe 출력 */}
+                <h2>Sever Data:</h2>
+                <h2>name:{data.name}</h2>
+                <h2>age:{data.age}</h2>
+            </>
+        );
+    }
+}
+
+export default App;
---
+++ learningmate-server/routes/test.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../src/database');
+
+//반드시 url 앞에 /빼먹지말것
+router.get('/test/data', (req, res) => {
+    db.query('SELECT * FROM person')
+        .then((result) => {
+            res.json({ message: 'Hello from the server!', resdata: result[0][0] });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+module.exports = router;
