29b82ac3f83d1454c243fade643c3b4a852824e9
mkhajiit <mkhajiit15@gmail.com>
2023-12-21 14:43:07 +0900

    서버에 회원가입구현

--- learningmate-server/routes/users.js
+++ learningmate-server/routes/users.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../src/database');
-const usersDao = require('../models/usersDAO');
+const usersController = require('../controllers/users');
 //회원가입, 로그인, 마이페이지는 여기에 구현하면 됩니다.
 router.post('/login', async (req, res) => {});
-router.post('/signup', async (req, res) => {
-    const userData = req.body;
-    try {
-        await usersDao.signUp(userData, (resp) => {
-            res.send(resp);
-        });
-    } catch (err) {
-        console.log(err);
-    }
-});
+router.post('/signup', usersController.signupUser);
 module.exports = router;
