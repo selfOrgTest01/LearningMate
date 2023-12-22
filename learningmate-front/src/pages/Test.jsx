3d803e259e013492b30b7328d2e2c9c6051d11f2
mkhajiit <mkhajiit15@gmail.com>
2023-12-22 23:56:57 +0900

    deploy test3

--- learningmate-front/src/pages/Test.jsx
+++ learningmate-front/src/pages/Test.jsx
@@ -7,7 +7,7 @@ function Test() {
     const [isloading, setLoading] = useState(true);
     //배포 서버 테스트 하실때  serverDomain을 사용하시면 됩니다 config.jsx에 설정해놨어요
     //로컬 서버 테스트 하실때는 localDomain을 사용하시면 됩니다
-    // const localDomain = 'http://localhost:8000';
+    // const localDomain = 'https://localhost:8000';
     const fn_get_data = useCallback(async () => {
         try {
             //데이터 get요청
@@ -25,7 +25,7 @@ function Test() {
     }, []);
     useEffect(() => {
         fn_get_data();
-    }, [fn_get_data]);
+    }, []);
     if (isloading) {
         return <div>Loading....</div>;
     } else {
@@ -35,8 +35,8 @@ function Test() {
                 {/* John Doe 출력 */}
                 <h2>Sever Data:</h2>
                 <h2>테스트용입니다~!!</h2>
-                <h2>name:{data.name}</h2>
-                <h2>age:{data.age}</h2>
+                <h2>email:{data.email}</h2>
+                <h2>nickname:{data.nickname}</h2>
             </>
         );
     }
