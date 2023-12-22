729129bd49b5ee1812965ae21c99176ed26f8824
mkhajiit <mkhajiit15@gmail.com>
2023-12-22 22:07:08 +0900

    회원가입 중복처리 로직추가

--- learningmate-front/src/pages/Signup.jsx
+++ learningmate-front/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Form, Container, Row, Col, Button, InputGroup } from 'react-bootstrap';
 function Signup() {
     const [data, setData] = useState({
@@ -8,6 +8,9 @@ function Signup() {
         password: '',
         nickname: '',
     });
+    const [duplicateEmail, setDuplicateEmail] = useState(false);
+    const [duplicatePhone, setDuplicatePhone] = useState(false);
+    const [duplicateNickname, setDuplicateNickname] = useState(false);
     //데이터를 읽어오는 함수
     const fn_read_data = useCallback(async () => {
         try {
@@ -27,29 +30,42 @@ function Signup() {
     //email중복확인
     const fn_check_email = useCallback(async () => {
         const result = await fn_read_data();
+
+        let isDuplicate = false;
+        //forEach 문에서는 return이나 break를 못쓴다 때문에 isDuplicate 변수를 따로 둬서 사용함
         result.data.data.forEach((item) => {
             if (item.email === data.email) {
-                window.alert('중복된 email입니다');
+                //중복이 있을때만 설정한 변수를 true로 바꾼다
+                isDuplicate = true;
             }
+            //없는경우엔 그대로 false
+            setDuplicateEmail(isDuplicate);
         });
     }, [data.email, fn_read_data]);
-
+    //휴대전화번호 중복확인
     const fn_check_phone = useCallback(async () => {
         const result = await fn_read_data();
+
+        let isDuplicate = false;
         result.data.data.forEach((item) => {
             if (item.phone_number === data.phone_number) {
-                window.alert('중복된 휴대전화번호입니다');
+                isDuplicate = true;
             }
+            setDuplicatePhone(isDuplicate);
         });
     }, [data.phone_number, fn_read_data]);
+    //닉네임 중복확인
     //useCallback에서 의존성 배열에 명시하지 않으면 해당 함수는 초기 렌더링 때 한 번만 생성되고,
     //이후에는 해당 함수가 참조하는 상태나 함수의 변경을 감지하지 않습니다. 즉, 초기 렌더링 시의 값들이 고정적으로 사용되게 됩니다.
     const fn_check_nickname = useCallback(async () => {
         const result = await fn_read_data();
+
+        let isDuplicate = false;
         result.data.data.forEach((item) => {
             if (item.nickname === data.nickname) {
-                window.alert('중복된 닉네임입니다');
+                isDuplicate = true;
             }
+            setDuplicatePhone(isDuplicate);
         });
     }, [data.nickname, fn_read_data]);
 
@@ -62,6 +78,7 @@ function Signup() {
         },
         [data]
     );
+
     return (
         <Container fluid style={{ backgroundColor: '#95a5a6', height: '100vh' }}>
             <Row className='justify-content-md-center'>
@@ -87,6 +104,7 @@ function Signup() {
                                 인증번호전송
                             </Button>
                         </InputGroup>
+
                         <Form.Group className='mb-3'>
                             <Form.Control type='text' placeholder='이메일인증번호' />
                         </Form.Group>
@@ -129,11 +147,20 @@ function Signup() {
                             />
                         </Form.Group>
                         <Form.Group className='mb-3' controlId='formGroupEmail'>
-                            <Button variant='primary' style={{ width: '100%' }} type='submit'>
+                            <Button
+                                variant='primary'
+                                style={{ width: '100%' }}
+                                type='submit'
+                                //중복되는게 하나라도 있다면 버튼이 disabled됩니다
+                                disabled={duplicateEmail || duplicatePhone || duplicateNickname}
+                            >
                                 등록
                             </Button>
                         </Form.Group>
                     </Form>
+                    {duplicateEmail && <p>-중복된 이메일입니다.</p>}
+                    {duplicatePhone && <p>-중복된 휴대전화번호입니다.</p>}
+                    {duplicateNickname && <p>-중복된 닉네임입니다.</p>}
                 </Col>
             </Row>
         </Container>
