dd40340b1aaad6ec49b28abc1cf19282798f4180
mkhajiit <mkhajiit15@gmail.com>
2023-12-23 00:05:03 +0900

    deploy test today final

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 반드시 .env파일을 learningmate-server에 넣고 실행시키세요
 
 # 프로젝트 시작
+
+deploy push할때는 git push origin deploy로 할 것
+배포 테스트는 반드시 deploy branch에서
--- learningmate-front/src/pages/Test.jsx
+++ learningmate-front/src/pages/Test.jsx
@@ -11,9 +11,7 @@ function Test() {
     const fn_get_data = useCallback(async () => {
         try {
             //데이터 get요청
-            const resp = await axios.get(
-                `https://port-0-learningmate-server-5r422alqajqbni.sel4.cloudtype.app/test/data`
-            );
+            const resp = await axios.get(`${serverDomain}/test/data`);
             console.log(resp.data);
             if (resp.data.resdata === false) window.alert('불러오기 실패');
             else setData((data) => ({ ...data, ...resp.data.resdata }));
@@ -25,7 +23,7 @@ function Test() {
     }, []);
     useEffect(() => {
         fn_get_data();
-    }, []);
+    }, [fn_get_data]);
     if (isloading) {
         return <div>Loading....</div>;
     } else {
