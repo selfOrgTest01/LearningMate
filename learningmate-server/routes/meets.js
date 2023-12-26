61f865b6c336d99969d190e35b824cfb56aa860f
mingyeong0802 <mkk0802@naver.com>
2023-12-26 17:47:54 +0900

    back) meets 테이블, api 구현

--- learningmate-server/routes/meets.js
+++ learningmate-server/routes/meets.js
@@ -1,48 +1,18 @@
-// 2023.12.22 수정 - 민경
+// 2023.12.26 수정 완료 - 민경
 
-/* eslint-disable new-cap */
 const express = require('express');
 const router = express.Router();
-
-const meetDAO = require('./../models/meetDAO');
+const meetsController = require('../controllers/meets');
 
 // 게시물 생성
-router.post('/insert', function(req, res, next) {
-  const body = req.body;
-  meetDAO.insert(body, (resp) => {
-    res.json(resp);
-  })
-});
-
+router.post('/insert', meetsController.meetInsert);
 // 게시물 수정
-router.put('/update', function (req, res, next) {
-  const body = req.body;
-  meetDAO.update(body, (resp) => {
-    res.json(resp);
-  });
-});
-
+router.post('/update', meetsController.meetUpdate);
 // 게시물 삭제
-router.delete('/delete/:meet_id', function (req, res, next) { 
-  const params = req.params;
-  meetDAO.delete(params, (resp) => {
-    res.json(resp);
-  });
-});
-
+router.delete('/delete/:meet_id', meetsController.meetDelete);
 // 전체 게시물 조회
-router.get('/meetList', function (req, res, next) { 
-  const query = req.query;
-  meetDAO.meetList(query, (resp) => {
-    res.send(resp);
-  })
-});
-
+router.get('/meetList', meetsController.meetList);
 // 게시물 상세
-router.get('/meet/:meet_id', function (req, res, next) { 
-  const params = req.params; 
-  meetDAO.meet(params, (resp) => {
-    res.send(resp);
-  })});
+router.get('/meet/:meet_id', meetsController.meet);
 
 module.exports = router;
