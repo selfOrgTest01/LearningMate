ec7a36118df6b2587e9c9ee9cd5ca7efa8877925
Nahyun Joo <dntwkg66@naver.com>
2023-12-30 10:11:37 +0900

    commit 2

--- learningmate-server/app.js
+++ learningmate-server/app.js
@@ -14,6 +14,7 @@ const app = express();
 const session = require('express-session');
 const courseRouter = require('./routes/courses'); // 나현 추가
 const commentRouter = require('./routes/comments'); // 나현 추가
+const bookmarkRouter = require('./routes/bookmark'); // 나현 추가
 const session = require("express-session");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -51,6 +52,7 @@ app.use('/reviews', reviewRouter); // 민경 추가
 
 app.use('/courses', courseRouter);  // 나현 추가
 app.use('/comments', commentRouter);  // 나현 추가
+app.use('/bookmark', bookmarkRouter);  // 나현 추가
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
