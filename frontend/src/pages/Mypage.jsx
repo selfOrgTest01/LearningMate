500a19ebc591be051faeffa0eb8317a35cd726ec
Nahyun Joo <dntwkg66@naver.com>
2024-01-04 15:08:13 +0900

    달력추가

--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,7 +1,23 @@
 // 12.27 나현 추가
+// 01.03 나현 추가 수정
+
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 function Mypage() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
+
   return (
-    <h1>Mypage</h1>
+    <div>
+      <h1>Mypage</h1>
+      <Calendar onChange={onChange} value={date} />
+    </div>
   );
 }
+
 export default Mypage;
