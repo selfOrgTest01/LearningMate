d44f58ce2033a483784ac6ce6d22ab3abd7bb99e
mingyeong0802 <mkk0802@naver.com>
2024-01-05 17:33:20 +0900

    front) 모임 디테일, 리스트 수정 작업

--- backend/controllers/participants.js
+++ backend/controllers/participants.js
@@ -25,7 +25,7 @@ exports.participantUpdate = async (req, res) => {
 };
 
 exports.participantDelete = async (req, res) => {
-  const { meet_id, participant_id } = req.params;
+  const {meet_id, participant_id} = req.params;
   try {
     await participantsDAO.participantDelete(meet_id, participant_id, (resp) => {
       res.send(resp);
@@ -36,12 +36,28 @@ exports.participantDelete = async (req, res) => {
 };
 
 exports.participantList = async (req, res) => {
-  const { meet_id } = req.params;
+  const {meet_id} = req.params;
   try {
     await participantsDAO.participantList(meet_id, (resp) => {
       res.send(resp);
     });
   } catch (err) {
     console.log(err);
+  }
+};
+
+// 다시 수정하기
+exports.getMeetParticipantsCount = async (req, res) => {
+  try {
+    const {meet_id} = req.params;
+    const result = await pool.query(
+      'SELECT COUNT(*) AS participant_count FROM meet_participants WHERE meet_id = $1 AND status = 1',
+      [meet_id]
+    );
+    const participantCount = result.rows[0].participant_count;
+    res.json({participant_count: participantCount});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: 'Internal Server Error'});
   }
 };
