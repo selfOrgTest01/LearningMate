f6de19ceef4f0e3fac40d4eb747cae212fb67ff0
Nahyun Joo <dntwkg66@naver.com>
2024-01-08 12:06:20 +0900

    달력수정

--- frontend/src/components/Mypage/Calendar.jsx
+++ frontend/src/components/Mypage/Calendar.jsx
@@ -1,17 +1,59 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './styles/Calendar.css';
 
 function MyCalendar() {
-  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [events, setEvents] = useState([]);
+
+  const addEvent = (date, content) => {
+    // 기존 이벤트에 새 이벤트 추가
+    setEvents([...events, { id: Date.now(), date, content }]);
+  };
+
+  const deleteEvent = (id) => {
+    // 해당 id를 가진 이벤트를 제외하고 새 이벤트 목록 설정
+    setEvents(events.filter((event) => event.id !== id));
+  };
+
+  const tileContent = ({ date }) => {
+    const eventForDate = events.find((event) => event.date.toDateString() === date.toDateString());
+
+    return (
+      <div>
+        {eventForDate && (
+          <>
+            <p>{eventForDate.content}</p>
+            <button className='delete-btn' onClick={() => deleteEvent(eventForDate.id)}>
+              삭제
+            </button>
+          </>
+        )}
+      </div>
+    );
+  };
 
   const onChange = (newDate) => {
-    setDate(newDate);
+    setSelectedDate(newDate);
   };
 
   return (
     <div>
-      <Calendar onChange={onChange} value={date} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const content = prompt('이벤트 내용을 입력하세요.');
+          if (content) {
+            addEvent(selectedDate, content);
+          }
+        }}
+      >
+        <Calendar onChange={onChange} value={selectedDate} tileContent={tileContent} />
+        <button className='add-btn' type='submit'>
+          일정 추가
+        </button>
+      </form>
     </div>
   );
 }
