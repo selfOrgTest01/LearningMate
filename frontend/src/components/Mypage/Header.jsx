8e89dd5032a878d7cdd0c554be8c025f43a5d815
Nahyun Joo <dntwkg66@naver.com>
2024-01-16 12:38:09 +0900

    마이페이지 Header 수정

--- frontend/src/components/Mypage/Header.jsx
+++ frontend/src/components/Mypage/Header.jsx
@@ -7,7 +7,7 @@ import LogoutFunction from '../../containers/Header/LogoutFunction';
 function Header() {
   const logoutUser = LogoutFunction();
   const navigate = useNavigate();
-  const auth = useSelector((state) => state.auth.isAuth);
+  // const auth = useSelector((state) => state.auth.isAuth);
 
   const handleLogoutClick = () => {
     logoutUser();
@@ -16,7 +16,7 @@ function Header() {
 
   return (
     <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-      <div className='container-fluid'>
+      <div className='container-fluid mx-5'>
         <Navbar.Brand href='/'>
           <img
             alt=''
@@ -37,50 +37,44 @@ function Header() {
         >
           <span className='navbar-toggler-icon'></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarColor04'>
+        <div className='collapse navbar-collapse mx-5' id='navbarColor04'>
           <ul className='navbar-nav me-auto'>
-            <li className='nav-item'>
+            <li className='nav-item mx-3'>
               <Link className='nav-link active' to='/'>
                 Home
                 <span className='visually-hidden'>(current)</span>
               </Link>
             </li>
-            <li className='nav-item'>
+            {/* <li className='nav-item'>
               <Link className='nav-link' to='/'>
                 검색
                 <span className='visually-hidden'>(current)</span>
               </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/meets'>
+            </li> */}
+            <li className='nav-item mx-3'>
+              <Link className='nav-link' to='/meetcategory'>
                 모임
                 <span className='visually-hidden'>(current)</span>
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className='nav-item mx-3'>
               <Link className='nav-link' to='/courses'>
                 강의
                 <span className='visually-hidden'>(current)</span>
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className='nav-item mx-3'>
               <Link className='nav-link' to='/mypage'>
                 마이페이지
                 <span className='visually-hidden'>(current)</span>
               </Link>
             </li>
           </ul>
-          <form className='d-flex'>
-            {auth ? (
-              <button className='btn btn-secondary my-2 my-sm-0' type='button' onClick={handleLogoutClick}>
-                로그아웃
-              </button>
-            ) : (
-              <Link className='nav-link' to='/mypage'>
-                마이페이지
-                <span className='visually-hidden'>(current)</span>
-              </Link>
-            )}
+          <form className='d-flex mx-4'>
+            {/* {auth ? ( */}
+            <button className='btn btn-secondary my-2 my-sm-0' type='button' onClick={handleLogoutClick}>
+              로그아웃
+            </button>
           </form>
         </div>
       </div>
